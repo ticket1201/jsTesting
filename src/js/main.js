9be8c47d7988009c3862a41a7adc2b1de3cbcdda
ticket1201 <ticket1201@gmail.com>
2021-08-25 19:09:17 +0300

    coded some :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 //here will by JavaScript code ...
+function pow(x, n) {
+    let result = 1;
+
+    if (n < 0) return NaN;
+    if (Math.round(n) != n) return NaN;
+
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+      
+    return result;
+}
+  
--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
-describe("pow", function() {
 
-    it("возводит в степень n", function() {
-      assert.equal(pow(2, 3), 8);
+describe("pow", function() {
+  describe("х в степени 3", function() {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+  });  
+
+  describe('проверка на ошибки', function() {
+
+    it("для отрицательных n возвращает NaN", function() {
+      assert.isNaN(pow(2, -1));
     });
   
-  });
+    it("для дробных n возвращает NaN", function() {
+      assert.isNaN(pow(2, 1.5));
+    });
+  
+  })
+});
